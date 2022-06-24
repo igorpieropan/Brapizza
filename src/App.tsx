fb4e4bcef3324ea8f884a098afb88d3a2cc09111
ipieropan <igorpieropan@gmail.com>
2022-06-24 23:30:42 +0200

    sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import * as FaIcons from "react-icons/fa";
+import Sidebar from "./components/sidebar/Sidebar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Router>
+                <Sidebar />
+                <Routes>
+                    <Route path="/"/>
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/sidebarData/SidebarData.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import * as BsIcons from "react-icons/bs";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+
+export const SidebarData = [
+    {
+      title: 'Menu',
+      path: '/',
+      icon: <AiIcons.AiFillHome />,
+      cName: 'nav-text'
+    },
+    {
+      title: 'Contact',
+      path: '/contact',
+      icon: <BsIcons.BsFillTelephoneFill />,
+      cName: 'nav-text'
+    }
+  ];
